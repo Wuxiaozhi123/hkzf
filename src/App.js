e39c57d8a86afb794bab765aff19a905a504d7dc
wuxiaozhi <1441174290@qq.com>
2020-04-05 20:01:45 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+// 1 引入 ant 组件
+import { Button } from "antd-mobile";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        {/* 2 使用 */}
+        <div>
+          <Button type="warning">按钮</Button>
+        </div>
+      </Fragment>
+    );
+  }
 }
-
 export default App;
